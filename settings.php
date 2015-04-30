18357151d742c7dc9481ae4f7a697050f83edaf8
neswill1 <nenadkonatar@live.ac.me>
2015-04-30 10:52:05 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+	$adresa[0]="3187 Western Row Rd, Maineville, OH, 45039"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="Maineville, OH, 45039";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="Maineville";
+	$telephone[0]=5132298318;// Broj koji se poziva na dugme CALL US
+
+
+	$companynumber=1;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
